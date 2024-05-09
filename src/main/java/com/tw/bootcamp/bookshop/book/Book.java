6aed2f6bd71f65bcb89a7badd000e18c03e862da
Puneet Jattana <puneet.jattana@thoughtworks.com>
2024-05-09 09:53:21 +0530

    [Puneet] added columns to Book

--- src/main/java/com/tw/bootcamp/bookshop/book/Book.java
+++ src/main/java/com/tw/bootcamp/bookshop/book/Book.java
@@ -1,10 +1,7 @@
 package com.tw.bootcamp.bookshop.book;
 
 import com.tw.bootcamp.bookshop.money.Money;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -13,6 +10,7 @@ import javax.persistence.*;
 @Builder
 @Getter
 @Entity
+@Setter
 @Table(name = "books")
 public class Book {
     @Id
@@ -22,6 +20,15 @@ public class Book {
     private String authorName;
     @Embedded
     private Money price;
+    private String imageUrl;
+    private String thumbnailUrl;
+    private Long bookCount;
+    private String isbn;
+    private String isbn13;
+    private Integer originalPublicationYear;
+    private String languageCode;
+    private Double averageRating;
+
 
     public BookResponse toResponse() {
         return BookResponse.builder()
